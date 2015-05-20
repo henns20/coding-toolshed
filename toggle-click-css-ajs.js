e29a53900f4220bc804514c713fc653167c21a92
henns20 <jjhennelly@gmail.com>
2015-05-19 21:33:46 -0400

    Update toggle-click-css-ajs.js

--- toggle-click-css-ajs.js
+++ toggle-click-css-ajs.js
@@ -1,1 +1,9 @@
 /* document the white-header toggle used with mbrco */
+
+// Look in index.html ng-class and then there has to be 
+
+/* <div id="main-nav navbar" class="navbar-collapse collapse"  collapse="status.navCollapsed">
+                        <ul class="nav navbar-nav navbar-right" >
+                            <li class=""><a href="#/" class="parent-nav" ng-init="white = true" ng-click="white = !white">White Header</a></li>
+                    
+*/
