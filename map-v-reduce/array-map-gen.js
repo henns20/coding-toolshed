0b31573c3fc53077bde35b3f84077f320bdf00e8
henns20 <jjhennelly@gmail.com>
2015-05-30 16:19:50 -0400

    Update array-map-gen.js

--- map-v-reduce/array-map-gen.js
+++ map-v-reduce/array-map-gen.js
@@ -1,3 +1,16 @@
+/** 
+ * whats the objective of a file like this - should go at the top. 
+ * Obj: make clear the logic and the difference between - map and reduce method
+ * Methods: studying the poly fills at mdn and trying out. 
+ * Diff(nutshell): 1. map - saves returned value from callback to mapped value; adds it to a new array; 
+ * returns the new array as output
+ * 2. reduce saves returned value in value variable - it provides that value and the next value 
+ * in the array as the args for you to return another value - but what ever you return inthe 
+ * end willl be returned as one final value
+ **/
+ 
+ 
+
 /**
 * map() method creates a new array with the results of calling a provided function on every element in this array.
 **/
