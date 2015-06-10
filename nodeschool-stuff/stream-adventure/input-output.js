842627759009157a751e0cfc7db79b937331de43
John Hennelly <jjhennelly@gmail.com>
2015-06-10 12:57:55 -0400

    stream-adventure

---
+++ nodeschool-stuff/stream-adventure/input-output.js
@@ -0,0 +1,9 @@
+/**
+ * Created by john on 6/10/15.
+ * Stream-adventure: Input Output ex 3 of ?
+ * Task take data from 'process.stdin' and pipe it to `process.stdout`
+ * With `.pipe()` & `process.stdn.pipe()` to be exact.
+ * Don't overthink this
+ */
+
+process.stdin.pipe(process.stdout);
