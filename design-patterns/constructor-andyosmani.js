5f34be30ddd786e6cbb500cf024e6b33589a06d4
John Hennelly <jjhennelly@gmail.com>
2015-10-29 09:13:42 -0400

    andy o constuctor

---
+++ design-patterns/constructor-andyosmani.js
@@ -0,0 +1,10 @@
+/* file is names after andy osmani - bc - I am only noting his view on a fundamental thing and making it in the context of the pattern discussion as a whole - by putting it in his book  about patterns 
+
+1. in classical oop: constructor is special method used to initialize a newly created object once memory has been alocated for it. 
+2. Object constructors used to create specific types of objects - which: 
+	both prepares the object for use 
+	and - accepting arguments which a constructor can use to set the values fo member properties and methos when the object is first created. 
+
+3. Object Creation section
+
+*/
