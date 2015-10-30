abcdc48edf26ab2813a70e8fb05d20e2858fbd92
John Hennelly <jjhennelly@gmail.com>
2015-10-30 09:43:37 -0400

    andy os constructor

--- design-patterns/constructor-andyosmani.js
+++ design-patterns/constructor-andyosmani.js
@@ -6,5 +6,19 @@
 	and - accepting arguments which a constructor can use to set the values fo member properties and methos when the object is first created. 
 
 3. Object Creation section
+	3 common ways:
+		1. var newObject = {};
+		2. var newObject = Object.create( Object.prototype );
+		3. var newObject = new Object(); 
+
+4. 4 ways to set and get key values 
+	Object.defineProperty & Object.defineProperties   is a new one for me as of ECMAScript 5
 
 */
+
+var person = Object.create( Object.prototype ); 
+console.log(person); 
+
+
+
+Object.defineProperty
