ea134d821d2b0f505d27266676dd40f7fec4cfbb
John Hennelly <jjhennelly@gmail.com>
2015-10-31 15:54:27 -0400

    object dot create

---
+++ design-patterns/objectdotcreate.js
@@ -0,0 +1,10 @@
+/* 
+the difference between Object.create vs new Object
+
+Mdn says: 
+var newObject = Object.create( someObject.prototype ) is equivalent to 
+var newObject = new ConstructorObject();  
+
+http://www.htmlgoodies.com/beyond/javascript/object.create-the-new-way-to-create-objects-in-javascript.html
+
+*/
