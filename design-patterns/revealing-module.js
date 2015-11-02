c6b76ba2b57ae70f2aebc5e76eae6371e0d7092d
John Hennelly <jjhennelly@gmail.com>
2015-11-02 07:51:19 -0500

    reveal pattern reference

--- design-patterns/revealing-module.js
+++ design-patterns/revealing-module.js
@@ -1,5 +1,12 @@
 /* Originated when Heilmann - 
-	1. didn't want to have to repeatedly call the name of the main object when we wanted to call one public 
+
+	1. All Functions and variables all Defined in the private scope and use pointers to the private functionality that we want to make public. 
+		- heilmann disliked 
+			islike the requiremen of the Module pattern habing to switch to object literal notation for thigns he wished to make public. 
+	2. Returns an anonymous object so don't have to the extra prefix.
+		
+		reasons:
+		didn't want to have to repeatedly call the name of the main object when we wanted to call one public 
 	method from another and public variables. 
 		* best described by Todd Motto - in how he points out the idea of Locally scoped Object Literal - represented by 
 			* var myObject = {};
@@ -8,9 +15,6 @@
 
 			and so...
 			Module.myObject.somePublicFunction()
-
-
-	2. dislike the requiremen of the Module pattern habing to switch to object literal notation for thigns he wished to make public. 
 
 Andy osmani 
 http://addyosmani.com/resources/essentialjsdesignpatterns/book/#revealingmodulepatternjavascript
@@ -22,22 +26,9 @@ http://toddmotto.com/mastering-the-module-pattern/
 */
 
 
-/* try without looking at the resources to mimic the reveal pattern 
-requirements: write an example of a reveal pattern
-
-1. first to do that write the structure of a standard module pattern... no? Or, Immediately Invoked Function Execution
-
-2. Typical of a reveal is the return of an anonymous object*  - Instead of a locally scoped object literal 
-
-3. public and private methods and data are both named and written outside of the returned object. References to the the public data is made in the returned objec
-
-*/
-
-// 1 
-
 (function() {
 
-//2. 
+
 
 var somePrivateVariable = 4;
 var somePublicVariable = 2;
