6d03a90dca7323e4891132d51d93d87766cd6372
John Hennelly <jjhennelly@gmail.com>
2015-11-03 11:43:41 -0500

    function jq

---
+++ fundamentals-js/program-stucture/functions/functions-jqs.js
@@ -0,0 +1,18 @@
+/* functions jq's
+
+1. function declaration patterns 
+ES5 
+
+a. function declaration notation
+	function f () {...}
+	one advantage: hoisting
+	 of this is the hoisting of function declarations in a script. The function can be declared lower in the program - but used in the beginning. Allowing for a better organization. 
+
+b. regular variable definiton  
+var f = function() {...}
+
+The call stack 
+
+
+*/
+
