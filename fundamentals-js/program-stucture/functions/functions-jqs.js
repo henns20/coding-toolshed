dd26eb1ba5e7cd18ab18542ebb33a38adf58bb58
henns20 <jjhennelly@gmail.com>
2015-11-09 23:34:10 -0500

    Update functions-jqs.js
    Update definition a function expression

--- fundamentals-js/program-stucture/functions/functions-jqs.js
+++ fundamentals-js/program-stucture/functions/functions-jqs.js
@@ -8,7 +8,7 @@ a. function declaration notation
 	one advantage: hoisting
 	 of this is the hoisting of function declarations in a script. The function can be declared lower in the program - but used in the beginning. Allowing for a better organization. 
 
-b. regular variable definiton  
+b. regular variable definiton  //  or  function expression
 var f = function() {...}
 
 The call stack 
