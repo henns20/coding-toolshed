4af458433e344f80b6cc7cd5b5548ddcf11461b6
John Hennelly <jjhennelly@gmail.com>
2015-11-23 06:14:12 -0500

    switch fizzbuzz

--- scratch.js
+++ scratch.js
@@ -1,27 +1,30 @@
-//count to 10 with recursion 2 ways 
-//1. with and if 
+/*
+Requirements: 
+1.create a script that outputs#1 know hundred.
+2. but with multiples three print out the word Fizz.
+3. with multiples of five output out the word buzz.
+4. with numbers of multiples of three and five output the word is FizzBuzz
+5. use the switch statement.
+*/
 
+function fizzBuzz() {
+var x, output;
+	for(x = 1;x < 101; x++) {
+	switch(true) {
+		case (!(x % 3) && !(x % 5) ):
+		console.log('FizzBuzz');
+		break;
+		case (!(x % 3)):
+		console.log('Fizz');
+		break;
+		case (!(x % 5)):
+		console.log('Buzz');
+		break;
+		default:
+		console.log(x);
+	}
 
-// function countDownIf10(num) {
-//   if(num < 0) {
-//     console.log('done');
-//     return; 
-//   }
-//   console.log(num);
-//   countDownIf10(num - 1);
-// }
-
-// countDownIf10(12);
-
-
-function countDownWhile(num) {
-  while (num > -1) {
-    console.log(num);
-    return countDownWhile(num - 1);   
-  }
-  
+	}
 }
 
-countDownWhile(12);
-
- 
+fizzBuzz();
